67e50d7005e52b927be09bf6891507e15546516b
Tanvir Mahmud <tmahmud1994@gmail.com>
2023-11-01 16:39:06 +0600

    Added Kabil's_syn_check.py file.

--- Kabil's_syn_check.py
+++ Kabil's_syn_check.py
@@ -12,18 +12,19 @@ from docx import Document
 
 doc_path = r"C:\Users\WALTON\OneDrive\Desktop\Tanvir\Vocabulary\1.docx" # .....................................................................
 
-def find_and_underline_word():
+def kabil_syn_search_capital():
     doc = Document(doc_path)
 
     for table in doc.tables:
         for row in table.rows:
-            serial_cell = row.cells[0].text
-            target_cell = row.cells[6]  # Assuming the 2nd cell contains the target word...........................................................
-            # target_word = target_cell.text.strip()
-            target_words = target_cell.text.strip().split()
+            word_serial_no = row.cells[0].text
+            target_word_cell = row.cells[6]  # Assuming the 2nd cell contains the target word...........................................................
+            # target_word = target_word_cell.text.strip()
+            target_words = target_word_cell.text.strip().split()
+            
             for target_word in target_words:
             
-                print(f'The target word is- {serial_cell}.{target_word}')
+                print(f'The target word is- {word_serial_no}.{target_word}')
 
                 for cell in row.cells[4:6]:  # Iterate through cells in the same row, starting from 2 to 2..............................................
                             # row.cells[start:end] will include the cell at index 'start' but not the cell at index 'end'.
@@ -49,9 +50,68 @@ def find_and_underline_word():
     # os.system(f'start {doc_path}')
 
 
-find_and_underline_word()
+# kabil_syn_search_capital()
 
 
 
 
 
+
+
+def kabil_syn_not_matched():
+    doc = Document(doc_path)
+
+    for table in doc.tables:
+        for row in table.rows:
+            word_serial_no = row.cells[0].text
+            target_word_cell = row.cells[6]  # Assuming the 2nd cell contains the target word...........................................................
+            # target_word = target_word_cell.text.strip()
+            target_words = target_word_cell.text.strip().split()
+            
+            
+            updated_kabil = []
+            
+            for target_word in target_words:
+                
+                x = 0
+
+                print(f'The target word is- {word_serial_no}.{target_word}')
+
+                for cell in row.cells[4:6]:  # Iterate through cells in the same row, starting from 2 to 2..............................................
+                            # row.cells[start:end] will include the cell at index 'start' but not the cell at index 'end'.
+                    for paragraph in cell.paragraphs:
+                        updated_text = []  # Create an empty list to store updated words
+                        for run in paragraph.runs:
+                            words = run.text.split()  # Split the text into words.
+                            for i, word in enumerate(words):
+                                
+                                if target_word.lower() in word.lower():
+                                    
+                                    # If the word matches, uppercase it
+                                    words[i] = word.upper()
+                                    # words[i] = word.upper() assigns the uppercase version of word back to the list of words (words) at the same index i where word was found.
+                                    
+                                    x += 1
+                                    
+                            updated_text.extend(words)  # Extend the list with updated words
+                        # Combine words back into a paragraph
+                        paragraph.text = ' '.join(updated_text) #
+            # After this loop, 'paragraph.text' will contain the updated text with the specified words in uppercase.
+                    
+                if x > 0:
+                    target_word = ""
+                    updated_kabil.extend([target_word])
+                else:
+                    updated_kabil.extend([target_word])
+                    
+            list_to_text = ', '.join(updated_kabil)
+
+            print(f"The outpurt result after search is---------- {list_to_text}.")
+            
+            target_word_cell.text = list_to_text
+
+    doc.save(doc_path)
+
+
+
+# kabil_syn_not_matched()
